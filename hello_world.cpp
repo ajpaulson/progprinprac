07a6685a0efa90471194cbaa8abc7035dff144e0
Alex Paulson <whileforkdofork@gmail.com>
2014-06-18 17:22:55 +0100

    added hello_world

---
+++ hello_world.cpp
@@ -0,0 +1,8 @@
+#include "std_lib_facilities.h"
+
+int main(int argc, char *argv[])
+{
+	cout << "Hello World!\n";		// output "Hello World!"
+	keep_window_open();				// wait for character to be entered
+	return 0;
+}
