a30e07f9249bcb5d92438a8c0c723b8627af5233
Alex Paulson <whileforkdofork@gmail.com>
2014-06-18 17:28:02 +0100

    ran hello_world through clang-format

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char *argv[])
 {
-	cout << "Hello World!\n";		// output "Hello World!"
-	keep_window_open();				// wait for character to be entered
-	return 0;
+  cout << "Hello World!\n"; // output "Hello World!"
+  keep_window_open();		// wait for character to be entered
+  return 0;
 }
