edb038e4976944343b457d631d909cf6703cf30d
Alex Paulson <whileforkdofork@gmail.com>
2014-06-19 11:11:38 +0100

    added unsafe1.cpp and unsafe2.cpp

---
+++ unsafe1.cpp
@@ -0,0 +1,14 @@
+#include "std_lib_facilities.h"
+
+int main(int argc, char *argv[])
+{
+  int a = 2000;
+  char c = a;     // try to squeeze a large int into a small char
+  int b = c;
+  if (a != b)     // != means "not equal"
+    cout << "oops!: " << a << "!=" << b << '\n';
+  else
+    cout << "Wow! We have large characters\n";
+
+  return 0;
+}
